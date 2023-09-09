f01fa634e8381d4f55338f2efa57dc9d52f68ca3
Rishabh Agrawal <rishabhag20@iitk.ac.in>
2023-09-09 16:22:15 +0530

    solution memoized

--- 494.target-sum.cpp
+++ 494.target-sum.cpp
@@ -9,23 +9,28 @@
 using namespace std;
 class Solution {
 private:
-    int solve(int index, int target, vector<int>& nums){
+    int solve(int index, int target, vector<int>& nums, vector<vector<int>>& dp){
+        if(target<-1000 || target>1000) return 0;
         if(index==0){
             if(target==0 && nums[0]==0) return 2;
             if(abs(target) == nums[0]) return 1;
             else return 0;
         }
 
-        // use -ve sign i.e., add to the target
-        int add = solve(index-1, target + nums[index] , nums);
-        int subtract = solve(index-1, target - nums[index], nums);
+        if(dp[index][target + 1000]!=-1) return dp[index][target+1000];
 
-        return add + subtract;
+        // use -ve sign i.e., add to the target
+        int add = solve(index-1, target + nums[index] , nums, dp);
+        int subtract = solve(index-1, target - nums[index], nums, dp);
+
+        return dp[index][target + 1000] = add + subtract;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
 
-        return solve(nums.size()-1, target, nums);
+        vector<vector<int>> dp(nums.size()+1, vector<int>(2001, -1));
+
+        return solve(nums.size()-1, target, nums, dp);
         
     }
 };
