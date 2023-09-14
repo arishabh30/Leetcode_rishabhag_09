3ab0ac825195fc08aa8db0d9d3b5e1f2ab80691e
Rishabh Agrawal <rishabhag20@iitk.ac.in>
2023-09-14 12:13:49 +0530

    Best time to buy and sell stock done -single for

---
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int profit = 0;
+        int maxProfit =0;
+        int minVal = prices[0];
+
+        for(int i=1;i<prices.size();i++){
+            profit = prices[i] - minVal;
+            maxProfit = max(profit, maxProfit);
+            minVal = min(minVal, prices[i]);
+        }
+
+        return maxProfit;        
+    }
+};
+// @lc code=end
+
